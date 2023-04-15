ff4f33f1d808347cbeea196684b89bc7499e5820
iMinusMinus <mean.leung@outlook.com>
2023-04-15 14:12:27 +0800

    try fix replit cannot open

--- shell.nix
+++ shell.nix
@@ -1,6 +1,13 @@
-{ pkgs }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {} }:
 
 pkgs.mkShell {
+  buildInputs = [
+    pkgs.graalvm17-ce
+    pkgs.maven
+    pkgs.replitPackages.jdt-language-server
+    pkgs.replitPackages.java-debug
+  ];
+
   shellHook = ''
     custom shell nix loaded
   '';
